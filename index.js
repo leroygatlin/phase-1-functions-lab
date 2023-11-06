d9abdf90f0f669d49ebc60bc6cdf2317c2a69b3b
leroygatlin <leroygatlin@gmail.com>
2023-11-06 13:05:36 -0800

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+// Code your solution in this file
+function distanceFromHqInBlocks(number){
+    if (number < 42){
+    return 42 - number    
+}
+return number - 42
+} 
+
+function distanceFromHqInFeet(number){
+
+    return distanceFromHqInBlocks(number) *264
+}
+
+function distanceTravelledInFeet(number1, number2){
+    if (number1 < number2){
+        return (number2 - number1) * 264  
+    }
+    return (number1 - number2) * 264
+}
+
+
+
+function calculatesFarePrice(start, destination){
+  let freeSample= 0;
+   let feet= distanceTravelledInFeet(start, destination)
+    if (feet <= 399){
+     return (freeSample)
+    }
+    else if (feet >= 401 && feet <= 2000){
+       return (2.56);
+    }
+
+    else if (feet >= 2001 && feet <= 2499){
+       return (25);
+    }
+    else if ( feet >= 2500){
+        return ('cannot travel that far');
+    }
+}
